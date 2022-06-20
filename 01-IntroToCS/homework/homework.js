a294e917b9c0f74306d2a8bb1077a2d3ad6b9516
JuanM <juanmanesevich25@gmail.com>
2022-06-20 20:28:58 -0300

    first hw

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var suma = 0;
+  for (var i=0; i<num.length; i++) {
+    suma = suma + num[num.length-1-i] * (2**i);
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var binario = (num%2).toString();
+  while (num>1) {
+    num = Math.floor(num/2);
+    binario = num%2 + binario;
+  }
+  return binario;
 }
 
 

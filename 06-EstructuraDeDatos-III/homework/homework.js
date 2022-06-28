c04c5908be3a0dc0087901b9243b159daec041c9
JuanM <juanmanesevich25@gmail.com>
2022-06-27 21:39:27 -0300

    sexta HW

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,60 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value=value;
+  this.left=null;
+  this.right=null;
+}
+
+BinarySearchTree.prototype.size = function() {
+  var contador = 1;
+  if (this.left!=null) {
+    contador++;
+    this.left.size();
+  }
+  if (this.right!=null) {
+    contador++;
+    this.right.size();
+  }
+  return contador;
+}
+
+BinarySearchTree.prototype.insert = function(nuevo) {
+  let arbol = new BinarySearchTree(nuevo);
+  if(nuevo<=this.value) {
+    if(this.left == null) {
+    this.left = arbol;
+    } else this.left.insert(nuevo);
+  }
+  if(nuevo>this.value) {
+    if(this.right==null) {
+      this.right=arbol;
+    }
+    else this.right.insert(nuevo);
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value) {
+  let left = this.left;
+  let right = this.right;
+  if (this.value == value) return true;
+  if (this.left) {
+   left = this.left.contains(value);
+  }
+  if (this.right) {
+    right = this.right.contains(value);
+  }  
+  if (left || right) return true;
+  else return false;
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function(orden) {
+
+}
+BinarySearchTree.prototype.breadthFirstForEach = function() {
+
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------

ae6020a192ec1166ff64f4db9e56d4fe88169113
JuanM <juanmanesevich25@gmail.com>
2022-06-29 15:50:31 -0300

    septima HW

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,36 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length<=1) return array;
+  var pivot = array[0];
+  var arrizq = [];
+  var arrder = [];
+  for (var i = 1; i < array.length; i++) {
+    if(pivot > array[i]) {
+       arrizq.push(array[i]);
+    }
+    else if (pivot <= array[i]) {
+      arrder.push(array[i])
+    }
+  }
+  return quickSort(arrizq).concat(pivot).concat(quickSort(arrder))
+}
 
+function merge (left,right) {
+  var leftIndex = 0;
+  var rightIndex = 0;
+  var arrayResult = [];
+
+  while(leftIndex < left.length && rightIndex < right.length) {
+    if(left[leftIndex] < right[rightIndex]) {
+      arrayResult.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      arrayResult.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return arrayResult.concat(left.slice(leftIndex).concat(right.slice(rightIndex)))
 }
 
 function mergeSort(array) {
@@ -14,7 +43,17 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length<2) return array;
+  var left =[];
+  var right = [];
+  for(var i = 0; i < array.length; i++) {
+    if(i<array.length/2) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i])
+    }
+  }
+  return merge(mergeSort(left),mergeSort(right));
 }
 
 // No modificar nada debajo de esta línea
